88e1c7bd8db7228003c15e23ea36d1671cf7e2a3
wata-pom <w.kumagai1224@gmail.com>
2019-11-07 11:10:13 +0900

    first commit

--- B-3_beautiful_kuku.py
+++ B-3_beautiful_kuku.py
@@ -7,3 +7,8 @@ beautiful_kuku.py を実装してください
 """
 rows = int(input("行数を入力してください:"))
 columns = int(input("列数を入力してください:"))
+
+for i in range(1, rows + 1):
+    for j in range(1, columns + 1):
+        print(f'{i} x {j} = {i * j:2d}', end=' | ')
+    print()
